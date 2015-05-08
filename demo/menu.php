e500c43a6d28b8604425054c0cfd0fdfa82339f8
dingdayu <614422099@qq.com>
2015-05-08 12:54:50 +0800

    增加消息发送的测试
    Signed-off-by: dingdayu <614422099@qq.com>

--- demo/menu.php
+++ demo/menu.php
@@ -3,17 +3,22 @@
 include './../XYWeChat.php';
 
 $arr = array(
-	array('id'=>1, 'pid'=>'', 'name'=>'获取信息', 'type'=>'click', 'code'=>'test'),
-	array('id'=>2, 'pid'=>'', 'name'=>'测试方法', 'type'=>'click', 'code'=>''),
-	array('id'=>3, 'pid'=>'', 'name'=>'关于框架', 'type'=>'about', 'code'=>''),
-	array('id'=>4, 'pid'=>'1', 'name'=>'获取图文', 'type'=>'click', 'code'=>'tuwen'),
-	array('id'=>5, 'pid'=>'1', 'name'=>'获多图文', 'type'=>'click', 'code'=>'duotuwen'),
-	array('id'=>6, 'pid'=>'1', 'name'=>'获取图片', 'type'=>'click', 'code'=>'getimg'),
-	array('id'=>7, 'pid'=>'2', 'name'=>'获取资料', 'type'=>'view', 'code'=>'http://www.xyser.com/XYWeChat/demo/oauth.php'),
-	array('id'=>8, 'pid'=>'2', 'name'=>'扫一扫', 'type'=>'scancode_push', 'code'=>'saosao'),
-	array('id'=>9, 'pid'=>'2', 'name'=>'扫一扫消息', 'type'=>'scancode_waitmsg', 'code'=>'scancode_waitmsg'),
-	array('id'=>10, 'pid'=>'2', 'name'=>'拍照', 'type'=>'pic_sysphoto', 'code'=>'pic_sysphoto'),
-	array('id'=>11, 'pid'=>'3', 'name'=>'发送位置', 'type'=>'location_select', 'code'=>'location_select'),
+	array('id'=>1, 'pid'=>'', 'name'=>'用户中心', 'type'=>'click', 'code'=>'test'),
+	array('id'=>2, 'pid'=>'', 'name'=>'活动中心', 'type'=>'click', 'code'=>''),
+	array('id'=>3, 'pid'=>'', 'name'=>'自助服务', 'type'=>'about', 'code'=>''),
+	
+	array('id'=>4, 'pid'=>'1', 'name'=>'注册邦定', 'type'=>'click', 'code'=>'tuwen'),
+	array('id'=>5, 'pid'=>'1', 'name'=>'实名认证', 'type'=>'click', 'code'=>'duotuwen'),
+	array('id'=>6, 'pid'=>'1', 'name'=>'修改信息', 'type'=>'click', 'code'=>'getimg'),
+	
+	array('id'=>7, 'pid'=>'1', 'name'=>'自驾游', 'type'=>'click', 'code'=>'getimg'),
+	array('id'=>8, 'pid'=>'1', 'name'=>'免费领取', 'type'=>'click', 'code'=>'getimg'),
+	array('id'=>9, 'pid'=>'2', 'name'=>'热门活动', 'type'=>'view', 'code'=>'http://www.xyser.com/XYWeChat/demo/oauth.php'),
+	array('id'=>10, 'pid'=>'2', 'name'=>'扫一扫', 'type'=>'scancode_push', 'code'=>'saosao'),
+	
+	array('id'=>11, 'pid'=>'2', 'name'=>'景区门票', 'type'=>'scancode_waitmsg', 'code'=>'scancode_waitmsg'),
+	array('id'=>12, 'pid'=>'2', 'name'=>'保险业务', 'type'=>'pic_sysphoto', 'code'=>'pic_sysphoto'),
+	array('id'=>13, 'pid'=>'3', 'name'=>'汽车保养', 'type'=>'location_select', 'code'=>'location_select'),
 );
 
 if($_GET['action'] == 'set'){
--- demo/sendmsg.php
+++ demo/sendmsg.php
@@ -1,13 +1,18 @@
 <?php
-
+ini_set("display_errors",1);
 include './../XYWeChat.php';
 
+
 $tousername = "oiRv4t0Rl9qVMT62yDDhtUmlYQC4";
+
+//发送客服文本
+if($_GET['action'] == 'kefuwenben'){
 //发送客服内容
-//$arr =  \XYWeChat\ResponseInitiative::text($tousername, '文本消息内容sdf');
+	$arr =  \XYWeChat\ResponseInitiative::text($tousername, '文本消息内容sdf');
 
-//print_r($arr);
+	print_r($arr);
 
+}elseif($_GET['action'] == 'kefutuwen'){
 		$item = array(
             'title'=>'title',
             'description'=>'description',
@@ -18,3 +23,25 @@ $tousername = "oiRv4t0Rl9qVMT62yDDhtUmlYQC4";
 		$test =  \XYWeChat\ResponseInitiative::news( $tousername, array($item));
 		
 		print_r($test);
+}elseif($_GET['action'] == 'uploadNews'){
+		 $neir = file_get_contents('html.txt');
+		//echo $neir;
+		$item = array(
+                            array('thumb_media_id'=>'_5yNFt1geWI0O195hppQ-cVuoZfUU5eEhEddkBFsQQooZgFQE0KiTy8scqfYPkNg' , 'author'=>'作者' . time(), 'title'=>'标题','content'=>  $neir , 'content_source_url'=>'www.lanecn.com', 'digest'=>'摘要', 'show_cover_pic'=>'1')
+				);
+		print_r($item);
+		$test =  \XYWeChat\AdvancedBroadcast::uploadNews( $item);
+		
+		print_r($test);
+		//YsshQHBKxFcrIEYtwWrn-bO5JQW-Yhi8xZNexWNR7MGYPAtHz5r6VNRE77QN7KE2
+		//m9C0hzxrpfG1X7iIqEchK7MV7_aqxQxQN_sGLcX0zYqjHxQMHk4Jc9j5tr0KCKzv
+}elseif($_GET['action'] == 'preview'){
+		//$neir = file_get_contents('html.txt');
+		 //echo $neir;
+		$item = "m9C0hzxrpfG1X7iIqEchK7MV7_aqxQxQN_sGLcX0zYqjHxQMHk4Jc9j5tr0KCKzv";
+
+		$test2 =  \XYWeChat\AdvancedBroadcast::preview($tousername, 'mpnews' , $test['media_id']);
+		
+		print_r($test2);
+		
+}
